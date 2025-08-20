f046fc62c81b304b8d7aae6977a21636d6303154
Logan Carpenter <loganofcarpenter@gmail.com>
2025-08-20 11:46:43 -0400

    More script files.

---
+++ Competition_Scripts/Simulation/run_num_changer.py
@@ -0,0 +1,6 @@
+def get_rn():
+	print(" ")
+	print("This sim's dumpling place is:\n")
+	print("    ","/home/export/lcarpent/CompuCell3D/output/Sim_Dumping_Ground/JAA_JBB_JAB__run_3")
+	print(" ")
+	return "/home/export/lcarpent/CompuCell3D/output/Sim_Dumping_Ground/JAA_JBB_JAB__run_3"
